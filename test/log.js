ace3850cf2b0a0825ec657f38d327f280a878c0f
valaxy <valaxy@qq.com>
2015-11-05 22:26:54 +0800

    fix bug: nothing show when output two tags

--- test/log.js
+++ test/log.js
@@ -1,11 +1,17 @@
 var log = require('../lib/log').openConsole()
 
-log.info('tag', 'this is info')
-log.warn('tag', 'this is warn')
-log.error('tag', 'this is error')
-log.debug('tag', 'this is debug')
+log.info('info', 'this is info')
+log.warn('warn', 'this is warn')
+log.error('error', 'this is error')
+log.debug('debug', 'this is debug')
 
-log.info('tag', 1, 'this is\nindent info')
-log.warn('tag', 1, 'this is\nindent warn')
-log.error('tag', 1, 'this is\nindent error')
-log.debug(null, 1, 'this is\nindent debug')
+log.info('info', 1, 'this is\nindent info')
+log.warn('warn', 1, 'this is\nindent warn')
+log.error('error', 1, 'this is\nindent error')
+log.debug('debug', 1, 'this is\nindent debug')
+
+
+// else
+log.info(null, 'no tag')
+log.info('tag', 'single tag')
+log.info(['tag1', 'tag2'], 'two tags')
